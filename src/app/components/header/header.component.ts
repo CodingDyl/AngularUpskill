1759ec0c1c599a69385fa269054941bb3772a5fc
DylanAbsa <dylan.petzer@absa.africa>
2023-07-13 17:25:45 +0200

    updated the ui to show date instead of title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,53 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Task Tracker';
+  myDate = new Date();
+
+  getMonth(): string {
+    switch ( this.myDate.getMonth() ) 
+    {
+      case 0:
+        return "January"
+        break;
+      case 1:
+        return "Febuary"
+        break;
+      case 2:
+        return "March"
+        break;
+      case 3:
+        return "April"
+        break;
+      case 4:
+        return "May"
+        break;
+      case 5:
+        return "June"
+        break;
+      case 6:
+        return "July"
+        break;
+      case 7:
+        return "August"
+        break;
+      case 8:
+        return "September"
+        break;
+      case 9:
+        return "October"
+        break;
+      case 10:
+        return "November"
+        break;
+      case 11:
+        return "December"
+        break;
+      default:
+        return "Unknown"
+    }
+  } 
+
+  title: string = this.myDate.getDate().toString() + " " + this.getMonth() + " " + this.myDate.getFullYear().toString();
   showAddTask: boolean = false;
   subscription!: Subscription;
 
